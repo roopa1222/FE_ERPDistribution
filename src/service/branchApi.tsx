9be7e01672afb3f608834a68cde806cbdca1d3b2
ritesh9372 <riteshyadav40229@gmail.com>
2024-11-20 21:58:01 +0530

    RY-dashboard

--- src/service/branchApi.tsx
+++ src/service/branchApi.tsx
@@ -11,3 +11,16 @@ export const getApi = async (url: string) => {
       throw error; // Rethrow the error to handle it where getApi is called
     }
   };
+  export const getBranchData = async (url: string, options: { params: any }) => {
+    try {
+      // Passing the params along with the request
+      const response = await userApi.get(`${ConfigData.SERVER_URL}${url}`, {
+        params: options.params,  // Pass params here
+      });
+      return response; // Return the response data
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error; // Rethrow the error to handle it in the calling function
+    }
+  };
+  
