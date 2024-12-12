d9280c04a6ca953bae3ede14dab839d30ec7c5d6
karthik-1725 <karthikganesan1725@gmail.com>
2024-12-12 23:13:49 +0530

    KG_feat-edit dsr invoice

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,24 +1,32 @@
-import userApi from "./axios"
-import ConfigData from "../config/constant.json";
+import userApi from './axios';
+import ConfigData from '../config/constant.json';
 
 export const getApi = async (url: string) => {
-    try {
-      const response = await userApi.get(`${ConfigData.SERVER_URL}${url}`);
-      return response; // Assuming you want the data from the response
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      throw error; // Rethrow the error to handle it where getApi is called
-    }
-  };
+  try {
+    const response = await userApi.get(`${ConfigData.SERVER_URL}${url}`);
+    return response; // Assuming you want the data from the response
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error; // Rethrow the error to handle it where getApi is called
+  }
+};
 
-
-  
 export const postApi = async (url: string, data: any) => {
-    try {
-      const response = await userApi.post(`${ConfigData.SERVER_URL}${url}`, data);
-      return response.data;
-    } catch (error) {
-      console.error("Error posting data:", error);
-      throw error;
-    }
-  };
+  try {
+    const response = await userApi.post(`${ConfigData.SERVER_URL}${url}`, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error posting data:', error);
+    throw error;
+  }
+};
+
+export const putApi = async (url: string, data: any) => {
+  try {
+    const response = await userApi.put(`${ConfigData.SERVER_URL}${url}`, data);
+    return response.data;
+  } catch (error) {
+    console.error('Error posting data:', error);
+    throw error;
+  }
+};
